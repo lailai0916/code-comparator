552a74b5bea3b3115c67ed405eb02eca86f1049d
lailai <lailai0x394@gmail.com>
2025-07-11 18:19:52 +0800

    upd

--- gen.cpp
+++ gen.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N=100000;
 random_device rd;
 mt19937 gen(rd());
 int Rand(int x,int y)
@@ -10,6 +11,11 @@ int Rand(int x,int y)
 }
 int main()
 {
-	// do sth
+	int n=Rand(1,N);
+	cout<<n<<'\n';
+	for(int i=1;i<=n;i++)
+	{
+		cout<<Rand(1,1000000)<<' ';
+	}
 	return 0;
 }
